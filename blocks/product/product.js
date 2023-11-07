6f5eefe605218956a47d70b414de27a59270a34f
Feng Xiao <feng.xiao@perficient.com>
2023-11-07 17:36:41 -0500

    clear comments

--- blocks/product/product.js
+++ blocks/product/product.js
@@ -1,4 +1,4 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
+//import { createOptimizedPicture } from '../../scripts/aem.js';
 
 let product = {
   image: '',
@@ -8,49 +8,6 @@ let product = {
   price: '',
   currency: '',
 };
-
-//
-// function getProductJson() {
-//   const jsonStr = `{
-//   "data": {
-//     "products": {
-//       "items": [
-//         {
-//           "name": "Galaxy A2-Bla-64 ",
-//           "sku": "Galaxy A2-Bla-64",
-//           "description": {
-//             "html": "Galaxy A23 5G makes keeping up with the things you love most a breeze with virtually lag free 5G. Relive life's best moments with a wide-lens camera and infinite display."
-//           },
-//           "image": {
-//             "url": "https://master-7rqtwti-f3ef32mfqsxfe.us-4.magentosite.cloud/media/catalog/product/cache/cd66438b6f093f936f90aed7cbc0bbf8/s/a/samsung-galaxy-a2-4gb-64gb-6.5-dual-sim.jpg",
-//             "label": "Galaxy A2-Bla-64 ",
-//             "position": null,
-//             "disabled": null
-//           },
-//           "stock_status": "IN_STOCK",
-//           "price_range": {
-//             "minimum_price": {
-//               "discount": {
-//                 "percent_off": 0,
-//                 "amount_off": 0
-//               },
-//               "final_price": {
-//                 "value": 729.99,
-//                 "currency": "USD"
-//               },
-//               "regular_price": {
-//                 "value": 729.99
-//               }
-//             }
-//           }
-//         }
-//       ]
-//     }
-//     }
-//     }`;
-//
-//   return JSON.parse(jsonStr);
-// }
 
 function getProduct(sku) {
   const graphqlEndPoint = 'https://master-7rqtwti-f3ef32mfqsxfe.us-4.magentosite.cloud/graphql?query=';
@@ -115,9 +72,6 @@ function getProduct(sku) {
       withCredentials: false,
     },
     success(data) {
-      console.log('---------data----->>>>>>');
-      console.log(data);
-      console.log('<<<<<<-----product---------');
       product = {
         image: data.data.products.items[0].image.url,
         description: data.data.products.items[0].description.html,
@@ -128,90 +82,13 @@ function getProduct(sku) {
       };
       // THIS WORKS!!
       console.log(product);
-      console.log('---------end---------');
     },
     error(data) {
-      console.error('Error:', 'Session authentication Failed');
+      console.error('Error:', 'Cannot load product');
       console.log('******** data=', data);
     },
   });
 }
-
-// async function getProduct(sku) {
-//   console.log('---------sku-----====' + sku);
-//   const graphqlEndPoint = 'https://master-7rqtwti-f3ef32mfqsxfe.us-4.magentosite.cloud/graphql?query=';
-//   const queryParam = `
-//         {
-//           products(filter: {sku: {in: ["${sku}"]}}) {
-//             items {
-//               name
-//               sku
-//               description {
-//                 html
-//               }
-//               image {
-//                 url
-//                 label
-//                 position
-//                 disabled
-//               }
-//               small_image {
-//                 url
-//                 label
-//                 position
-//                 disabled
-//               }
-//               thumbnail {
-//                 url
-//                 label
-//                 position
-//                 disabled
-//               }
-//               stock_status
-//               price_range {
-//                 minimum_price {
-//                   discount {
-//                     percent_off
-//                     amount_off
-//                   }
-//                   final_price {
-//                     value
-//                     currency
-//                   }
-//                   regular_price {
-//                     value
-//                   }
-//                 }
-//               }
-//             }
-//           }
-//         }`;
-//
-//   const productQuery = graphqlEndPoint + queryParam;
-//
-//   const response = await fetch(productQuery, {
-//     Method: 'GET',
-//     Headers: {
-//       Accept: 'application.json',
-//       'Content-Type': 'application/json',
-//     },
-//   });
-//   const data = await response.json();
-//   console.log('---------data----->>>>>>');
-//   console.log(data);
-//   console.log('<<<<<<-----product---------');
-//   product = {
-//     image: data.data.products.items[0].image.url,
-//     description: data.data.products.items[0].description.html,
-//     name: data.data.products.items[0].name,
-//     stock: data.data.products.items[0].stock_status,
-//     price: data.data.products.items[0].price_range.minimum_price.final_price.value,
-//     currency: data.data.products.items[0].price_range.minimum_price.final_price.currency,
-//   };
-//   // THIS WORKS!!
-//   console.log(product);
-//   console.log('---------end---------');
-// }
 
 export default function decorate(block) {
   /* change to ul, li */
@@ -221,9 +98,6 @@ export default function decorate(block) {
     console.log(`Loading product ${sku}...`);
     // Get product information from graphQL:
     getProduct(sku);
-
-    console.log('++++++NAME+++++++');
-    console.log(product.name);
 
     const li = document.createElement('li');
 
@@ -243,7 +117,7 @@ export default function decorate(block) {
     ul.append(li);
   });
 
-  // ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  //ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
 }
